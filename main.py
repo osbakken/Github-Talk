9947c3e5891ed80226f3e6f9207d5da4bf956cea
osbakken <54602276+osbakken@users.noreply.github.com>
2020-10-23 13:53:09 -0700

    Update main.py
    Added subtraction functionality

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import sys
 def add(a, b):
     return a + b
 
+def sub(a, b):
+    return a - b
+
 
 if __name__ == '__main__':
     a, b = map(int, sys.argv[1:])
     print(f"The sum of {a} and {b} is {add(a,b)}")
+    print(f"The diffence of {a} and {b} is {sub(a,b)}")
